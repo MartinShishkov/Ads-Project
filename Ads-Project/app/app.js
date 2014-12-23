e1624badfa6b7c628a1c851ead91f4970d531c0f
MartinShishkov <martin.shishkov@hotmail.com>
2014-12-23 22:39:00 +0200

    Created angular app. Created home controller. Added a new view which handles home page.

---
+++ Ads-Project/app/app.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="C:\Users\Martin\Documents\Ads-Project\Ads-Project\index.html" />
+(function() {
+    var app = angular.module('AdsProject', ["ngRoute"]);
+    console.log("Success: App created.");
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "./views/main.html",
+                controller: "homeController"
+            })
+            .otherwise({redirectTo: "/"});
+
+    });
+
+}());
---
+++ Ads-Project/app/controllers/HomeController.js
@@ -0,0 +1,9 @@
+﻿(function() {
+    var module = angular.module('AdsProject');
+
+    var homeController = function() {
+        console.log("In home controller");
+    }
+
+    module.controller('homeController', homeController);
+}());
