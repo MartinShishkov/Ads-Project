d9007d2f594bac3e924d0ba0e3e6454a305b7a6e
MartinShishkov <martin.shishkov@hotmail.com>
2014-12-27 14:29:55 +0200

    Added fully functional pagination for the ads.

--- Ads-Project/app/controllers/HomeController.js
+++ Ads-Project/app/controllers/HomeController.js
@@ -6,6 +6,8 @@
         var pageSize = 2;
         var startPage = 1;
 
+        $scope.activePage = startPage;
+
         DataQueryExecutor.getAds(pageSize, startPage).then(onAdsLoad);
 
         DataQueryExecutor.getCategories().then(onCategoriesLoad);
@@ -14,7 +16,19 @@
 
         function onAdsLoad(result) {
             $scope.ads = result.data.ads;
+            $scope.pages = loadPages(result.data.numPages);
         };
+
+        function loadPages(numPages) {
+            var pages = [];
+            for (var i = 0; i < numPages; i++) {
+                pages[i] = {
+                    title: (i + 1).toString(),
+                    pageNumber: i + 1
+                };
+            }
+            return pages;
+        }
 
         function onCategoriesLoad(result) {
             $scope.categories = result.data;
@@ -23,6 +37,41 @@
         function onTownsLoad(result) {
             $scope.towns = result.data;
         }
+
+        // Pagination logic
+        $scope.goToPage = function(pageNumber) {
+            $scope.activePage = pageNumber;
+            DataQueryExecutor.getAds(pageSize, pageNumber).then(onAdsLoad);
+        }
+
+        $scope.isActivePage = function (pageNumber) {
+            return $scope.activePage == pageNumber;
+        }
+
+        $scope.goToFirstPage = function() {
+            $scope.activePage = 1;
+            $scope.goToPage($scope.activePage);
+        }
+
+        $scope.goToLastPage = function () {
+            $scope.activePage = $scope.pages.length;
+            $scope.goToPage($scope.activePage);
+        }
+
+        $scope.onePageBack = function() {
+            if ($scope.activePage > 1) {
+                $scope.activePage--;
+                $scope.goToPage($scope.activePage);
+            }
+        }
+
+        $scope.onePageForward = function () {
+            if ($scope.activePage < $scope.pages.length) {
+                $scope.activePage++;
+                $scope.goToPage($scope.activePage);
+            }
+        }
+        //////////////////////////////////////////////
     }
 
     module.controller('homeController', homeController);
