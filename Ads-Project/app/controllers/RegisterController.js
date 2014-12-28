05d3e84390c13399a90c78279a9e18ebf7604679
MartinShishkov <martin.shishkov@hotmail.com>
2014-12-28 14:44:10 +0200

    Created a service to handle user actions. Registration implemented.

--- Ads-Project/app/controllers/RegisterController.js
+++ Ads-Project/app/controllers/RegisterController.js
@@ -1,8 +1,27 @@
 ﻿(function () {
     var module = angular.module('AdsProject');
 
-    var registerController = function () {
-        console.log("In register controller");
+    var registerController = function ($scope, UserQueryExecutor, Auth) {
+
+        $scope.attemptRegister = function() {
+            var user = {
+                username: $scope.registerUsername,
+                password: $scope.registerPassword,
+                confirmPassword: $scope.registerPasswordConfirm,
+                name: $scope.registerName,
+                email: $scope.registerEmail,
+                phone: $scope.registerPhone,
+                townId: null,
+            }
+
+            UserQueryExecutor.registerUser(user)
+                .then(function () {
+                console.log("User account created.");
+            }, function(error) {
+                console.log(error);
+            });
+        }
+
     }
 
     module.controller('registerController', registerController);
---
+++ Ads-Project/app/services/UserQueryExecutor.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    var module = angular.module('AdsProject');
+
+    var UserQueryExecutor = function ($http, rootUrl) {
+        var registerUser = function(user) {
+            return $http.post(rootUrl + 'user/register', user);
+        }
+
+        var getUserProfile = function() {
+            
+        }
+
+        var editUserProfile = function() {
+            
+        }
+
+        var getAds = function (pageSize, startPage) {
+            return $http.get(rootUrl + "ads?pagesize=" + pageSize + "&startpage=" + startPage);
+        };
+
+        var getCategories = function () {
+            return $http.get(rootUrl + "categories");
+        }
+
+        var getTowns = function () {
+            return $http.get(rootUrl + "towns");
+        }
+
+        return {
+            registerUser: registerUser,
+            getUserProfile: getUserProfile,
+            editUserProfile: editUserProfile
+        };
+    }
+
+    module.factory('UserQueryExecutor', UserQueryExecutor);
+}());
