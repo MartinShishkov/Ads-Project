047536549989f18a1276207c8d0a050f620d25f9
MartinShishkov <martin.shishkov@hotmail.com>
2014-12-30 22:59:21 +0200

    Added user ads filtering by status.

--- Ads-Project/app/controllers/UserAdsController.js
+++ Ads-Project/app/controllers/UserAdsController.js
@@ -4,6 +4,27 @@
     var userAdsController = function ($scope, $rootScope, DataQueryExecutor, MessageProvider) {
         console.log("In user ads controller.");
 
+        $scope.adsFilters = [
+            {
+                title: "All",
+                filter: ""
+            },
+            {
+                title: "Published",
+                filter: "Published"
+            },
+            {
+                title: "Waiting Approval",
+                filter: "WaitingApproval"
+            },
+            {
+                title: "Inactive",
+                filter: "Inactive"
+            }
+        ];
+
+        $scope.activeAdFilter = $scope.adsFilters[0];
+
         DataQueryExecutor.getUserAds()
             .then(function(result) {
             loadUserAds(result);
@@ -18,12 +39,22 @@
             DataQueryExecutor.deactivateAd(id)
                 .then(function (result) {
                 MessageProvider.success("Ad deactivated successfully.");
-                console.log(result);
             }, function(error) {
                 MessageProvider.error(error.data.message);
             });
 
         }
+
+        $scope.isActiveFilter = function (filterTitle) {
+            return filterTitle == $scope.activeAdFilter.title;
+        }
+
+        $scope.filterAdsBy = function (filter) {
+            console.log(filter);
+            $scope.activeAdFilter = filter;
+        }
+
+
     }
 
     module.controller('userAdsController', userAdsController);
